4dbbeb4c5d9d23025ef010e26b25346005cc869d
zll <zll>
2021-11-07 23:11:04 -0500

    add lambda functions

---
+++ index-photos/lambda_function.py
@@ -0,0 +1,72 @@
+import json
+import boto3
+import requests 
+import requests_aws4auth
+import datetime
+import chardet
+import idna
+import urllib3
+import certifi
+
+bot = boto3.client('s3')
+
+def lambda_handler(event, context):
+    s3_info = event['Records'][0]['s3']
+    bucket_name = s3_info['bucket']['name']
+    key_name = s3_info['object']['key']
+    
+    labels = getRekLabel(bucket_name, key_name)
+    print("---------labels from Reko -------",labels)
+    parsed_photo = parsePhoto(bucket_name, key_name, labels)
+    print("---------this is parsed photo-------",parsed_photo)
+    reply = uploadToES(parsed_photo)
+    print("---------this is reply from ES-------", reply)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully indexed!')
+    }
+
+# function to generate label for current photo 
+def getRekLabel(bucket_name, key_name):
+    boto = boto3.session.Session('','',region_name='us-east-1')
+    rekognition = boto.client('rekognition')
+    response = rekognition.detect_labels(
+        Image = {
+            'S3Object' : {
+                'Bucket' : bucket_name,
+                'Name' : key_name
+            }
+        },
+        MaxLabels=10
+    )
+    labels=[]
+    for res in response['Labels']:
+        labels.append(res['Name'])
+    
+    # Get S3 metadata using headObject() method
+    headObject = bot.head_object(Bucket=bucket_name, Key=key_name)
+    # assert 'Metadata' in headObject, "Oops! Forget to add metadata(custom field) while uploading imgs."
+    print(headObject)
+    metaData = headObject['Metadata']
+    if len(metaData) !=0:
+        custom_labels = metaData['customlabels'].split(',')
+        labels.extend(custom_labels)
+    return labels
+    
+# function to parse current photo in order to store in ES
+def parsePhoto(bucket_name, key_name, labels):
+    parsed_photo = {
+        'objectKey': key_name,
+        'bucket': bucket_name,
+        'createdTimeStamp': datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S"),
+        'labels': labels
+    }
+    return parsed_photo
+    
+# function to connect to ES and upload photo 
+def uploadToES(parsed_photo):
+    endpoint = 'https://search-photos-qxhwqporwm5u6sqzzema4xdt6a.us-east-1.es.amazonaws.com/photos/photo'
+    headers = { "Content-Type": "application/json" }
+    response = requests.post(endpoint, auth=("photoalbum", "Photoalbum123!"), json=parsed_photo, headers=headers)
+    return response 
+    
---
+++ search-photos/lambda_function.py
@@ -0,0 +1,69 @@
+import json
+import boto3 
+import requests
+import idna
+import requests_aws4auth
+import chardet
+import urllib3
+import certifi
+
+def lambda_handler(event, context):
+    query = event['queryStringParameters']['q']
+    labels = getLabels(query)
+    print("----------labels is----------", labels)
+    photo_paths = getPhotoPaths(labels)
+    return{
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,x-api-key',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+        },
+        'body': json.dumps(photo_paths)
+    }
+
+# function to get labels 
+def getLabels(query):
+    lex = boto3.client('lex-runtime')
+    response = lex.post_text(
+        botName='SearchBot',  
+        botAlias='Test',
+        userId="string",           
+        inputText=query
+    )
+    
+    labels=[]
+    print("--------lambda response", response)
+    if 'slots' not in response:
+        print('No photo collection for query {}'.format(query))
+    else:
+        slot_val = response['slots']
+        for key,value in slot_val.items():
+            if value!=None:
+                labels.append(value)
+    return labels
+    
+# function to get all photo paths  
+def getPhotoPaths(labels):
+    photo_paths=[]
+    for l in labels:
+        host = 'https://search-photos-qxhwqporwm5u6sqzzema4xdt6a.us-east-1.es.amazonaws.com/photos'
+        path = host + '/_search?q=labels:'+ l 
+        headers = { "Content-Type": "application/json" }
+        response = requests.get(path, headers=headers, auth=('photoalbum', 'Photoalbum123!'))
+        response = response.json()
+        for res in response['hits']['hits']:
+            img_bucket = res['_source']['bucket']
+            img_key_name = res['_source']['objectKey']
+            
+            # need to check the link format 
+            img_url = 'https://s3.amazonaws.com/' + str(img_bucket) + '/' + str(img_key_name)
+            if img_url not in photo_paths:
+                photo_paths.append(img_url)
+    return photo_paths
+  
+        
+        
+        
+        
